3bcf12f0c41a8d384d7df0b00e76ed8d69c46501
huongnbm <89367878+huongnbm@users.noreply.github.com>
2021-08-24 14:24:31 +0700

    Add files via upload

--- giaiptbac2.rb
+++ giaiptbac2.rb
@@ -1,4 +1,5 @@
 class PhuongTrinhBacHai
+	attr_reader :a, :b, :c
 	def initialize(a,b,c)
 		@a=a
 		@b=b
@@ -7,20 +8,20 @@ class PhuongTrinhBacHai
 	@k=-1
 	private
 		def tinh_delta
-			@delta = @b*@b - 4*@a*@c
+			@delta = b*b - 4*a*c
 		end
 	public
 		def giai_pt
 			delta = send :tinh_delta
 			if @a != 0 then
 				if delta > 0  then
-					x1 = (-@b + Math.sqrt(delta))/(@a*2)
-					x2 = (-@b - Math.sqrt(delta))/(@a*2)
-					puts "x1: #{x1} - x2: #{x2}"
+					@k=1
+					@x1 = (-b + Math.sqrt(delta))/(a*2)
+					@x2 = (-b - Math.sqrt(delta))/(a*2)
 				else 
 					if delta == 0 then
-						x = -@b/(@a*2)
-						puts "PT nghiệm kép x1 = x2 = : #{x}"
+						@k=0
+						@x = -b/(a*2)
 					end
 				end
 			else
@@ -29,7 +30,10 @@ class PhuongTrinhBacHai
 		end
 	public 
 		def ket_qua
-				if @k==1  then
+				if (@x1 == nil) && (@x == nil) then
+					puts "ban can chay phan giai pt"
+				else
+					if @k==1  then
 					puts "x1: #{@x1} - x2: #{@x2}"
 				else 
 					if @k==0 then
@@ -44,6 +48,7 @@ class PhuongTrinhBacHai
 						end
 					end
 				end
+			end
 		end
 
 end
@@ -56,5 +61,5 @@ b = b.to_i
 c = gets()
 c = c.to_i
 run = PhuongTrinhBacHai.new(a,b,c)
-run.giai_pt
+
 run.ket_qua
